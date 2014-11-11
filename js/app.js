614ce8d6f7c8f556bac277dbe60d76c6d75bd537
nmorduch <nmorduch@oberlin.edu>
2014-11-11 09:50:49 -0500

    make all games solvable

--- js/app.js
+++ js/app.js
@@ -9,10 +9,33 @@ $(function() {
 	 	$board.append('<div class="row">');
 	 	$row = $board.children().last();
 		for (j=0;j<5;j++) {
-			on = Math.random() < 0.5 ? " on" : "";
+			on = Math.random() < 0.5 ? "" : "";
+			// on = Math.random() < 0.5 ? " on" : "";
 		 	$row.append('<div class="cell'+on+'" data-r="'+i+'" data-c="'+j+'"></div>');
 		}
 	}
+
+	// Turn on some lights
+	var lightToggle = function($cell){
+		var r, c, check;
+		r = $cell.data('r');
+		c = $cell.data('c');
+		$('.cell').each(function() {
+			var $this, r2, c2;
+			$this = $(this);
+			r2 = $this.data('r');
+			c2 = $this.data('c');
+			if ((r2-r<2 && r-r2<2 && c2===c)||(c2-c<2 && c-c2<2 && r2===r)){
+				$this.toggleClass('on');
+			}
+		});
+	};
+	$('.cell').each(function() {
+		if (Math.random() < 0.5) {
+			lightToggle($(this));
+		}
+	});
+
 
 	// Size the board
 	var boardSize = function(){
@@ -36,19 +59,8 @@ $(function() {
 
 	// Allow game play
 	$('.cell').click(function() {
-		var $this, r, c, check;
-		$this = $(this);
-		r = $this.data('r');
-		c = $this.data('c');
-		$('.cell').each(function() {
-			var $this, r2, c2;
-			$this = $(this);
-			r2 = $this.data('r');
-			c2 = $this.data('c');
-			if ((r2-r<2 && r-r2<2 && c2===c)||(c2-c<2 && c-c2<2 && r2===r)){
-				$this.toggleClass('on');
-			}
-		});
+		var $this = $(this);
+		lightToggle($this);
 		// Check if game over
 		if (!$this.hasClass('on')) {
 			check = false;
